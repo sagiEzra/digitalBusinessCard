3b7228af04878bba69cd6255ef143dc367aa7a80
sagiezra <sagiezra0@gmail.com>
2025-03-14 13:19:50 +0200

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      console.log("Applying rewrite rules...");
-      return [
-        {
-          source: "/:path*", 
-          has: [{ type: "host", value: "mad-kor.co.il" }],  
-          destination: "/madkor/:path*",  
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: "/:path*", 
+        has: [{ type: "host", value: "mad-kor.co.il" }],
+        destination: "https://ezracards.co.il/madkor/:path*",  
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
